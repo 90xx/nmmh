15d61aec36a1e0e9f6761c303dfa34529cc0a258
90xx <1489715627@qq.com>
2026-06-10 10:50:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -75,7 +75,7 @@ async function loadComicsAutomatically() {
 }
 
 // ✅ 关键：在这里填写你的漫画文件夹名！
-const COMIC_FOLDERS = ['life线上的我们']; // ← 修改这里！
+const COMIC_FOLDERS = ['雾蓝色的雨后晴天']; // ← 修改这里！
 
 /**
  * 获取用户配置的漫画文件夹名列表
